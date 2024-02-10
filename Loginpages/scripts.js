51e97e0b8cef8799ce7baf69d60c1c61b0a0c79c
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-02-10 21:17:39 +0545

    cg

---
+++ Loginpages/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const studentLoginForm = document.getElementById('student-login-form');
+    const teacherLoginForm = document.getElementById('teacher-login-form');
+
+    // You can implement server-side
